93f6ab61b54349e4332dbea3b8c13e3674d7327a
Guanglie <guanglie.zhu@us.bosch.com>
2021-05-31 20:40:56 -0500

    Add project files.

---
+++ DotNetCoreMvcWebApp/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using DotNetCoreMvcWebApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotNetCoreMvcWebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            this._logger.LogDebug("HomeController - Index - " + DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"));
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            this._logger.LogWarning("HomeController - Privacy - " + DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"));
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            this._logger.LogError("HomeController - Error - " + DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"));
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ myDotNetCoreWebApp/Pages/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace myDotNetCoreWebApp.Pages
+{
+    public class HomeController : Controller
+    {
+        private ILogger logger;
+        public IActionResult Index(ILogger<HomeController> _log)
+        {
+            this.logger = _log;
+            this.logger.LogWarning("I am in Home Index View");
+
+            return View();
+        }
+    }
+}
